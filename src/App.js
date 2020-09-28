2cfba75f00ca1f3c8ce0ee1ad80ecf424ca405a8
Leandro Andrade de Oliveira <leandro@live.com.my>
2020-09-28 19:36:17 -0300

    Jogo da Velha finalizado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const calculateWinner = squares => {
+    const winnerLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winnerLines.length; i++) {
+        const [a, b, c] = winnerLines[i];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+            return squares[a];
+    }
+
+    return null;
 }
 
-export default App;
+const Square = ({ value, onClick }) => {
+    return (
+        <button className="square" onClick={ onClick }>
+            { value }
+        </button>
+    );
+}
+
+const Board = () => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [nextPlayer, setNextPlayer] = useState('X');
+    const [winner, setWinner] = useState(null);
+
+    const handleClick = (i) => {
+        const newSquares = squares.slice();
+
+        if (squares[i] || winner)
+            return;
+
+        newSquares[i] = nextPlayer;
+
+        setSquares(newSquares);
+        setNextPlayer(nextPlayer === 'X' ? 'O' : 'X');
+
+        setWinner(calculateWinner(newSquares));
+    }
+
+    const renderSquare = (i) => {
+        return <Square
+            value={ squares[i] }
+            onClick={ () => handleClick(i) }
+        />;
+    }
+
+    const status = winner ? 'Winner: ' + winner : `Next player: ${ nextPlayer }`;
+
+    return (
+        <div>
+            <div className="status">{ status }</div>
+            <div className="board-row">
+                { renderSquare(0) }
+                { renderSquare(1) }
+                { renderSquare(2) }
+            </div>
+            <div className="board-row">
+                { renderSquare(3) }
+                { renderSquare(4) }
+                { renderSquare(5) }
+            </div>
+            <div className="board-row">
+                { renderSquare(6) }
+                { renderSquare(7) }
+                { renderSquare(8) }
+            </div>
+        </div>
+    )
+}
+
+const Game = () => {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board/>
+            </div>
+
+            <div className="game-info">
+                <div>{/* status */ }</div>
+                <ol>{/* TODO */ }</ol>
+            </div>
+        </div>
+    );
+};
+
+export default Game;
